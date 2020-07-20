5dce43a62a8ad68a892f661b25d92d9cdaf923f6
ATDoicin <atdoicin@gmail.com>
2020-07-20 05:46:18 +0100

    woop

--- untitled0.py
+++ untitled0.py
@@ -9,12 +9,10 @@ Created on Wed Jul  1 10:53:47 2020
 
 
 import numpy as np
-from scipy import *
 import matplotlib.pyplot as plt
-import main_script as m
 import wrapper as w
 import time
-import blockwiseview
+
 
 i = complex(0,1)
 
@@ -22,30 +20,21 @@ wm = 1.1e7
 gm = 32
 k = 2e5
 d0 = 1.1e7
-g0_list = np.linspace(200,1000,5)
+g0_list = np.linspace(0.1,10000,100)
 g2_list = np.linspace(0,0,1)
 n = 237.5
-ep = 6e8
+ep = 5e8
 ##
 start = time.time()
 ##
-r_arr, cov_arr = w.prep_qfi(wm, gm, k, d0, n, ep, g0_list, g2_list)
-#qfi_output = w.single_qfi(r_arr, cov_arr, g0_list)
-print(np.around(cov_arr[0,:][0],4))
-print(np.around(r_arr[0,:][0],4))
+r_arr, cov_arr, a_sq = w.prep_qfi(wm, gm, k, d0, n, ep, g0_list, g2_list)
+qfi_output = w.single_qfi(r_arr, cov_arr, g0_list)
+rel_output = w.rel_error(qfi_output,g0_list)
 ##
 
 
-
-print(np.around([r_arr[0,:][ii] for ii in range(len(g0_list))],6))
-print(np.around(np.gradient([r_arr[0,:][ii] for ii in range(len(g0_list))],1,axis=0),6))
-
-#print(qfi_output)
+print(qfi_output)
 print('Done in ' + str(time.time()-start) + ' seconds.')
 ##use np.block() function to bring together then trace, also this is becoming overcomplicatedly slow :(
-test = np.reshape(np.arange(4*4),(4,4))
-print(test)
-print(blockwiseview.blockwise_view(test,(2,2)))
-print(np.reshape(np.ones(4),(2,2)))
-print(np.matmul(np.reshape(np.ones(4),(2,2)), blockwiseview.blockwise_view(test,(2,2))))
-print(np.matmul(blockwiseview.blockwise_view(test,(2,2)), np.reshape(np.ones(4),(2,2))))
+
+plt.scatter(a_sq,rel_output)
--- wrapper.py
+++ wrapper.py
@@ -8,12 +8,21 @@ Created on Mon Jul 13 16:17:13 2020
 #!usr/bin/python
 
 import numpy as np
-from numpy import linalg
 import main_script
-from timeit import default_timer as timer 
-from numba import jit
 import blockwiseview
 
+def block_mult(a, b):
+    res = np.zeros([a.shape[0], b.shape[1]],np.ndarray)
+    for l in range(a.shape[0]):
+        for m in range(b.shape[1]):
+            res[l][m] = sum([np.dot(a[l][ii], b[ii][m]) for ii in range(a.shape[1])])
+    return res
+                
+def easyblock(arr):
+    return np.block([[arr[0][0],arr[0][1],arr[0][2],arr[0][3]],
+                     [arr[1][0],arr[1][1],arr[1][2],arr[1][3]],
+                     [arr[2][0],arr[2][1],arr[2][2],arr[2][3]],
+                     [arr[3][0],arr[3][1],arr[3][2],arr[3][3]]])
 
 def find_cov(wm, gm, k, d0, n, ep, g0_list, g2_list):
     cov_array = np.zeros([len(g2_list),len(g0_list)],np.ndarray)
@@ -42,12 +51,14 @@ def find_x(wm, gm, k, d0, n, ep, g0_list, g2_list):
 def prep_qfi(wm, gm, k, d0, n, ep, g0_list, g2_list):
     r_array = np.zeros([len(g2_list),len(g0_list)],np.ndarray)
     cov_array = np.zeros([len(g2_list),len(g0_list)],np.ndarray)
+    alpha_squared_array = np.zeros([len(g2_list),len(g0_list)])
     for l in range(len(g2_list)):
         for m in range(len(g0_list)):
             temp_obj = main_script.Little_r(wm, gm, k, d0, g0_list[m], ep, g2_list[l], n)
             cov_array[l,m] = temp_obj.solve_for_cov_from_initial()
             r_array[l,m] = temp_obj.r
-    return r_array, cov_array
+            alpha_squared_array[l,m] = 0.5*(np.real(r_array[l,m][2])**2 + np.real(r_array[l,m][3])**2)
+    return r_array, cov_array, alpha_squared_array
 
 def single_qfi(r_arr, cov_arr, g0_list):
     i = complex(0,1)
@@ -64,8 +75,12 @@ def single_qfi(r_arr, cov_arr, g0_list):
         temp_L_cov = np.kron(temp_cov, temp_cov)
         middle_bit = np.linalg.pinv(4*temp_L_cov + L_w)
         part_a = np.dot(r_diff_arr[ii], np.dot(np.linalg.pinv(temp_cov), r_diff_arr[ii]))
-        print(middle_bit.shape)
-        print(temp_cov.shape)
-        part_b = 2*np.trace(np.matmul(cov_diff_arr[ii],np.matmul(middle_bit,cov_diff_arr[ii])))
+        part_b = 2*np.trace(easyblock(block_mult(cov_diff_arr[ii],block_mult(blockwiseview.blockwise_view(middle_bit,(4,4)),cov_diff_arr[ii]))))##broken
         qfi_output_arr[ii] = part_a + part_b
     return qfi_output_arr
+
+def rel_error(qfi_arr, g0_array):
+    rel_arr = np.zeros(len(g0_array))
+    for ii in range(len(g0_array)):
+        rel_arr[ii] = qfi_arr[ii]**-0.5 * g0_array[ii]**-1
+    return rel_arr
